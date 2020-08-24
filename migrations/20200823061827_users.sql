80e154c9a119d6e6d0aa49876167c1701ebe5072
Igor Halfeld <hello@igorluiz.me>
2020-08-24 06:36:19 -0300

    fix ident

--- migrations/20200823061827_users.sql
+++ migrations/20200823061827_users.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE users (
-	id            TEXT PRIMARY KEY UNIQUE,
-	telegram_id   INTEGER UNIQUE,
-	name          TEXT NOT NULL,
-	username TEXT NOT NULL UNIQUE,
-	clothing_type TEXT NOT NULL,
-  started_at    TEXT 
+	id              TEXT PRIMARY KEY UNIQUE,
+	telegram_id     INTEGER UNIQUE,
+	name            TEXT NOT NULL,
+	username        TEXT NOT NULL UNIQUE,
+	clothing_type   TEXT NOT NULL,
+  started_at      TEXT 
 );
 
 -- +goose StatementEnd
